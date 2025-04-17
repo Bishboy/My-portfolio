b2ca658e731ad82812a545be420cf62c06a40705
Bishboy <bishboy2spieking@gmail.com>
2025-04-17 17:43:11 +0100

    fixes

--- src/Component/Footer.tsx
+++ src/Component/Footer.tsx
@@ -6,39 +6,40 @@ import logo from "../assets/img/logo.svg";
 
 const Footer = () => {
   return (
-    <footer className="  ">
-      <div className="w-[90%] mx-auto flex flex-col md:flex-row gap-[3rem] justify-between  px-4 pt-[10rem] ">
-        <div>
-          <img src={logo} alt="" />
-        </div>
-        <div className="flex flex-col gap-4">
-          <div className="flex justify-between ">
-            <a
-              href="#"
-              className="w-[42px] h-[42px] relative group rounded-full duration-500 ease-in-out z-10 flex items-center justify-center border-2 shadow-lg overflow-hidden"
-            >
-              <div className="absolute inset-0 bg-purple-500/60 rounded-full scale-0 group-hover:scale-100 transition-transform duration-500 ease-in-out z-0"></div>
-
-              <img src={Navicon1} alt="" className="relative z-10" />
-            </a>
-            <a
-              href="#"
-              className="w-[42px] h-[42px] relative group rounded-full duration-500 ease-in-out z-10 flex items-center justify-center border-2 shadow-lg overflow-hidden"
-            >
-              <div className="absolute inset-0 bg-purple-500/60 rounded-full scale-0 group-hover:scale-100 transition-transform duration-500 ease-in-out z-0"></div>
-
-              <img src={Navicon2} alt="" className="relative z-10" />
-            </a>
-            <a
-              href="#"
-              className="w-[42px] h-[42px] relative group rounded-full duration-500 ease-in-out z-10 flex items-center justify-center border-2 shadow-lg overflow-hidden"
-            >
-              <div className="absolute inset-0 bg-purple-500/60 rounded-full scale-0 group-hover:scale-100 transition-transform duration-500 ease-in-out z-0"></div>
-
-              <img src={Navicon3} alt="" className="relative z-10" />
-            </a>
+    <footer className="bg-[#121212] w-full py-8 relative z-20">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col md:flex-row gap-8 justify-between items-center">
+          <div>
+            <img src={logo} alt="Logo" className="h-10" />
           </div>
-          <p className="text-white  mt-[10rem]">&copy; 2024. All Rights Reserved.</p>
+          <div className="flex flex-col items-center md:items-end gap-4">
+            <div className="flex gap-4">
+              <a
+                href="#"
+                className="w-[42px] h-[42px] relative group rounded-full duration-500 ease-in-out z-10 flex items-center justify-center border-2 border-white/20 shadow-lg overflow-hidden hover:border-purple-500"
+              >
+                <div className="absolute inset-0 bg-purple-500/60 rounded-full scale-0 group-hover:scale-100 transition-transform duration-500 ease-in-out z-0"></div>
+                <img src={Navicon1} alt="LinkedIn" className="relative z-10" />
+              </a>
+              <a
+                href="#"
+                className="w-[42px] h-[42px] relative group rounded-full duration-500 ease-in-out z-10 flex items-center justify-center border-2 border-white/20 shadow-lg overflow-hidden hover:border-purple-500"
+              >
+                <div className="absolute inset-0 bg-purple-500/60 rounded-full scale-0 group-hover:scale-100 transition-transform duration-500 ease-in-out z-0"></div>
+                <img src={Navicon2} alt="Facebook" className="relative z-10" />
+              </a>
+              <a
+                href="#"
+                className="w-[42px] h-[42px] relative group rounded-full duration-500 ease-in-out z-10 flex items-center justify-center border-2 border-white/20 shadow-lg overflow-hidden hover:border-purple-500"
+              >
+                <div className="absolute inset-0 bg-purple-500/60 rounded-full scale-0 group-hover:scale-100 transition-transform duration-500 ease-in-out z-0"></div>
+                <img src={Navicon3} alt="Instagram" className="relative z-10" />
+              </a>
+            </div>
+            <p className="text-white/70 text-sm mt-4">
+              &copy; {new Date().getFullYear()}. All Rights Reserved.
+            </p>
+          </div>
         </div>
       </div>
     </footer>
