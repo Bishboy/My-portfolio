5b146c9b778298767af0b1b5e3ccca277bac523b
Bishboy <bishboy2spieking@gmail.com>
2025-05-21 18:08:22 +0100

    link fixed

--- src/Component/Project.tsx
+++ src/Component/Project.tsx
@@ -68,7 +68,7 @@ const data: Items[] = [
     description:
       "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ips",
     image: beach,
-    link: "https://github.com/Bishboy/beaches",
+    link: "https://beaches-sigma.vercel.app/",
   },
   {
     id: 4,
